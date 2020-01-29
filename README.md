fbc7e77a336142e347d70a5af2ace20b3fe5415d
mbozja <bozjamojca@gmail.com>
2020-01-29 21:57:55 +0100

    Initial project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Forkify food project
+
+Forkify web app.  
+
+First run "npm i" to install all the packages.  
+To run the project in watch mode for development run "npm run start".  
+To build project run "npm run build_dev" or "npm run build_prod".  
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.scss";
+// Global app controller
+import num from './test';
+
+
+console.log(`I imported ${num} from another module!`);
+
+
+
+let myVar = "Test text";
+let myVar_2 = "Test text 2";
+console.log(myVar);
+console.log(myVar_2);
+
