871d86dbe5ccb890cd276452b24212eeb68f4631
mbozja <bozjamojca@gmail.com>
2020-01-30 11:02:43 +0100

    import modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import "./styles.scss";
-// Global app controller
-import num from './test';
 
-
-console.log(`I imported ${num} from another module!`);
-
-
-
-let myVar = "Test text";
+/* let myVar = "Test text";
 let myVar_2 = "Test text 2";
 console.log(myVar);
-console.log(myVar_2);
+console.log(myVar_2); */
 
+// Lecture ES6 modules
+import str from './models/Search';
+import {add, multiply} from './views/searchView';
+
+console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
