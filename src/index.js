3e023ffb79dd72a8d55f7a737de9544b1ece4ed6
mbozja <bozjamojca@gmail.com>
2020-01-30 14:32:37 +0100

    Imported from searchView.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import "./styles.scss";
+import str from './js/models/Search.js';
+import { add, multiply, ID } from './js/views/searchView.js';
 
-/* let myVar = "Test text";
+/*
+
+Absolutna pot: 
+c:/Mojca/Projekt/datoteka.txt
+
+Relativna (se gleda iz trenutnega direktorija v katermu si):
+Trenutno je ta file v src mapi.
+in trenutni direktorij se oznaci z ./
+en nivo visi gres pa z ../ , dva nivoja visje pa ../../ in tko naprej
+sepravi ce hoces it v js/models napises:
+./js/models
+
+*/ 
+
+let myVar = "Test text";
 let myVar_2 = "Test text 2";
 console.log(myVar);
-console.log(myVar_2); */
+console.log(myVar_2); 
 
 // Lecture ES6 modules
-import str from './models/Search';
-import {add, multiply} from './views/searchView';
-
 console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
+console.log("a dela? : ", str);
+
+
