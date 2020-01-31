b6eae1afb7935a0aa6fbe797eab9cf509082e7b6
mbozja <bozjamojca@gmail.com>
2020-01-31 20:42:05 +0100

    Import axios and async function getResults()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.scss";
 // Lecture ES6 modules
-import str from './js/models/Search';
-import { add as a, multiply as m, ID } from './js/views/searchView';
+//import str from './js/models/Search';
+//import { add as a, multiply as m, ID } from './js/views/searchView';
+//import * as searchView from './js/views/searchView';
+
+// Project imports:
+import axios from 'axios';
 
 /*
 
@@ -16,16 +20,47 @@ sepravi ce hoces it v js/models napises:
 ./js/models
 
 */ 
-
+/*
 let myVar = "Test text";
 let myVar_2 = "Test text 2";
 console.log(myVar);
 console.log(myVar_2); 
+*/
 
 // Lecture ES6 modules
 //console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`); ali pa:
-console.log(`Using imported functions! ${a(ID, 2)} and ${m(3,5)}. ${str}`);
+//console.log(`Using imported functions! ${a(ID, 2)} and ${m(3,5)}. ${str}`);
+//console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
 
 //console.log("a dela? : ", str);
+
+
+// Project code:
+/**
+ * This method accepts parameter "query".
+ */
+async function getResults(query) {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`); 
+    console.log(res);
+}
+getResults('pizza'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
