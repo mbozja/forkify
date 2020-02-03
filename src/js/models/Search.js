abaebe94336d8ab61236b7428dc17cb43562e065
mbozja <bozjamojca@gmail.com>
2020-02-03 09:26:18 +0100

    Moved code from index.js here, changed async getResults

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,2 +1,20 @@
 //export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`); 
+            this.recipes = res.data.recipes;
+            console.log(recipes);
+        } catch (error) {
+            alert(error);
+        }
+        
+    }
+}
 
