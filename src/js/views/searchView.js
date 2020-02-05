0a7beaf019ac3d622206d8d0a7f24a34662ed710
mbozja <bozjamojca@gmail.com>
2020-02-05 09:40:25 +0100

    Correct order of createButton HTML code

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
+    elements.searchResPages.innerHTML = '';
 }
 
 /*
@@ -58,10 +59,10 @@ const renderRecipe = recipe => {
 // type: 'prev' or 'next'
 const createButton = (page, type) => `
     <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
         <svg class="search__icon">
             <use href="assets/img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
         </svg>
-        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
     </button>
 `;
 
