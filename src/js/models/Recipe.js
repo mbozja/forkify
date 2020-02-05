9616960bb09f3dbd11a76c29e51925371af4a138
mbozja <bozjamojca@gmail.com>
2020-02-05 10:16:06 +0100

    async getRecipe

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,8 +8,7 @@ export default class Recipe {
     async getRecipe() {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
-            this.result = res.data.recipes;
-            //console.log(this.result);
+            console.log(res);
         } catch(error) {
             console.log(error);
         }
