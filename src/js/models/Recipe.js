a84478213cc403a040b933ecb1a3eb489ea29fd2
mbozja <bozjamojca@gmail.com>
2020-02-05 11:18:02 +0100

    Added calcTime and calcServings in class Recipe

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,9 +8,24 @@ export default class Recipe {
     async getRecipe() {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
-            console.log(res);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
         } catch(error) {
             console.log(error);
         }
+    }
+
+    calcTime() {
+        // Asumming that we need 15 min for each ingredients
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 39);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.servings = 4;
     }
 }
