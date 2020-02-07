31c0646deb91cc50b0364b58c2002e6babfe9cb5
mbozja <bozjamojca@gmail.com>
2020-02-07 17:18:02 +0100

    Imported listView, added controlList  and event listener on shopping list

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Recipe from './js/models/Recipe';
 import List from './js/models/List';
 import * as searchView from './js/views/searchView';
 import * as recipeView from './js/views/recipeView';
+import * as listView from './js/views/listView';
 import { elements, renderLoader, clearLoader } from './js/views/base';
 
 /*
@@ -151,6 +152,39 @@ const controlRecipe = async () => {
 
  ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
  
+/** 
+ * LIST CONTROLLER 
+ */
+const controlList = () => {
+    // Create a new list IF there is none yet
+    if (!state.list) state.list = new List();
+
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+
+        // Delete from UI
+        listView.deleteItem(id);
+
+    // Handle the count update 
+    } else if (e.target.matches('.shopping__count--value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
  // Handling recipe button clicks
  elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -163,6 +197,8 @@ const controlRecipe = async () => {
         // Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controller
     }
  });
 
