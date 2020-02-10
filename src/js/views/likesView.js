66b4affb84f9666c8b8a25c24215df37ce72872a
mbozja <bozjamojca@gmail.com>
2020-02-10 11:42:40 +0100

    Fixed iconString

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -1,7 +1,7 @@
 import { elements } from './base';
 
 export const toggleLikeBtn = isLiked => {
-    const iconString = isLiked ? 'icon-heaart' : 'icon-heart-outline';
+    const iconString = isLiked ? 'icon-heaart' : 'icon-heart-outlined';
     document.querySelector('.recipe__love use').setAttribute('href',`/assets/img/icons.svg#${iconString}`);
     //icon.svg#icon-heart-outlined
 }
