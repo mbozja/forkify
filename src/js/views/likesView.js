0aa4d4f563dc40c4b48ce0bbc50bd469dd6526d0
mbozja <bozjamojca@gmail.com>
2020-02-11 09:27:10 +0100

    Added toggleLikeMenu and renderLike

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -5,3 +5,23 @@ export const toggleLikeBtn = isLiked => {
     document.querySelector('.recipe__love use').setAttribute('href',`/assets/img/icons.svg#${iconString}`);
     //icon.svg#icon-heart-outlined
 }
+
+export const toggleLikeMenu = numLikes => {
+elements.likesMenu.style.visibility = numLikes > 0 ? 'visible' : 'hidden';
+};
+
+export const renderLike = like => {
+    const markup = `
+        <li>
+            <a class="likes__link" href="#${like.id}">
+                <figure class="likes__fig">
+                    <img src="${like.img}" alt="${like.title}">
+                </figure>
+                <div class="likes__data">
+                    <h4 class="likes__name">${like.title}</h4>
+                    <p class="likes__author">${like.author}</p>
+                </div>
+            </a>
+        </li>
+    `;
+}
